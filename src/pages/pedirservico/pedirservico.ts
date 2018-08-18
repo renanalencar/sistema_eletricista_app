1108087843d4473fdb72f5c2b9eb2faca7993085
Pedro Mauro <pedromauro216@gmail.com>
2018-08-18 20:17:51 -0300

    back pedir serviço

--- src/pages/pedirservico/pedirservico.ts
+++ src/pages/pedirservico/pedirservico.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, AlertController, Alert } from 'ionic-angular';
+import { ClienteProvider } from '../../providers/cliente';
+import { Chamado } from '../../classes/chamado';
+
 
 /**
  * Generated class for the PedirservicoPage page.
@@ -15,7 +18,56 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PedirservicoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public enderecopassado: String;
+  public endereco: String = null;
+  public descricao: String = null;
+  public servico: Chamado;
+
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              private providerCliente: ClienteProvider,
+              private loadingCtrl: LoadingController,
+              private _alertCtrl: AlertController
+            ) {
+    
+    let loader = this.loadingCtrl.create({
+      content: 'Carregando. Aguarde...'
+    });
+    loader.present();
+    loader.dismiss();//fiz isso pra quando precisarmos usar o loader
+
+  }
+
+
+  enviarServico(){
+    if(!this.endereco || !this.descricao) {            
+      this._alertCtrl.create({
+        title: 'Preenchimento obrigatório',
+        subTitle: 'Você deve preencher todas as informações',
+        buttons: [{ text: 'Ok'}]
+      }).present();
+      return;
+    }
+
+    let loader = this.loadingCtrl.create({
+      content: 'Carregando. Aguarde...'
+    });
+    loader.present();
+
+    this.providerCliente
+      .solicitarServico(this.servico)
+      .then(resposta => {
+        loader.dismiss();
+        //mudar para proxima pagina
+      })
+      .catch(()=> {
+        loader.dismiss();
+        this._alertCtrl.create({
+          title: 'Problema no envio do servico',
+          subTitle: 'Tente novamente em alguns minutos',
+          buttons: [{ text: 'Ok'}]
+        }).present();
+      })
   }
 
   ionViewDidLoad() {
