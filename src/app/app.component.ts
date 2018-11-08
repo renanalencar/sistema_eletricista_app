2f420bb9d8966fc0dd52629dbb27f71c7bc98c16
Marcos Sá <marcos.sa@polijunior.com.br>
2018-11-07 23:20:05 -0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -40,7 +40,7 @@ import { Events } from 'ionic-angular';
 })
 export class MyApp {
 
-  rootPage:any = PerfilPage;
+  rootPage:any = PerfilEletPage;
 
   nome : string = "oi medeiros";
   foto : string = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Deputados_cabo_Daciolo_%28PSOL-RJ%29_e_Marcos_Reategui_%28PSC-AP%29_participam_do_programa_Brasil_em_Debate_%28cropped%29.jpg/200px-Deputados_cabo_Daciolo_%28PSOL-RJ%29_e_Marcos_Reategui_%28PSC-AP%29_participam_do_programa_Brasil_em_Debate_%28cropped%29.jpg";
